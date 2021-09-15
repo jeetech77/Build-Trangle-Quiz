7a58efd89304b52022fd45d94cbfa4f03d23248b
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2021-09-15 20:15:48 +0530

    Fix : Negative values errors

--- GussTheAngle/script.js
+++ GussTheAngle/script.js
@@ -2,20 +2,21 @@ const userInput = document.querySelectorAll(".input-box");
 const btnSumbit = document.querySelector('.btn-submit');
 const outputPara = document.querySelector(".output")
 btnSumbit.addEventListener("click", () => {
-    let sum = 0;
-    userInput.forEach(element => {
-        let temp = Number(element.value);
-        if (temp === 0) {
-            sum = 0;
+    let firstAngle = Number(userInput[0].value);
+    let secondAngle = Number(userInput[1].value);
+    let thirdAngle = Number(userInput[2].value);
+    sum = firstAngle + secondAngle + thirdAngle;
+    if (firstAngle > 0 && secondAngle > 0 && thirdAngle > 0) {
+        if (sum === 180) {
+            outputPara.textContent = "Yay, These angles will make a traingle."
+        } else if (sum === 0) {
+            outputPara.textContent = "Please enter valid values.";
         } else {
-            sum += temp;
+            outputPara.textContent = "Oops, These angles can't make a traingle";
         }
-    })
-    if (sum === 180) {
-        outputPara.textContent = "Yay, These angles will make a traingle."
-    } else if (sum === 0) {
-        outputPara.textContent = "Please enter valid values.";
-    } else {
-        outputPara.textContent = "Oops, These angles can't make a traingle";
+    }
+    else {
+        outputPara.textContent = "Enter values grater then 0.";
+        outputPara.style.borderColor = 'red';
     }
 })
